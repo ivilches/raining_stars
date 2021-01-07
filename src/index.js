a2cc46e6c38644fbc977f5f669a4cdd761a3b497
Israel Vilches <vilchesvalle@gmail.com>
2021-01-07 20:34:05 +0100

    Imported scenes through barrel.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,19 @@
-import Phaser from 'phaser';
-import sky from './assets/sky.png';
-import star from './assets/star.png';
-
-class MyGame extends Phaser.Scene {
-    constructor() {
-        super();
-    }
-
-    preload() {
-        this.load.image('sky', sky);
-        this.load.image('star', star);
-    }
-
-    setBackground() {
-        this.add.image(400, 300, 'sky');
-    }
-
-    create() {
-        this.setBackground();
-    }
-
-    update() { }
-}
+import 'phaser';
+import { WelcomeScene, GameScene, ScoreScene } from './scenes';
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 300 },
-            debug: false
-        }
-    }
+  title: 'Raining stars',
+  type: Phaser.AUTO,
+  parent: 'game',
+  width: 800,
+  height: 600,
+  scene: [WelcomeScene, GameScene, ScoreScene],
+  physics: {
+      default: 'arcade',
+      arcade: {
+          debug: false
+      }
+  }
 };
 
 const game = new Phaser.Game(config);
